0d2c00986ef9cb555e0884f4892db47e580307ef
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-14 17:11:45 +0900

    Create UserDAO.java

---
+++ src/user/UserDAO.java
@@ -0,0 +1,60 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn;	//db에 접근하는 객체
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?"; 
+		//?에 ID를 넣어줄 수 있게끔 한다 실제로 존재하는지도 비교
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword))
+					return 1; //로그인 성공
+				else
+					return 0; //비밀번호 불일치
+			}
+			return -1; //아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES(?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserPhoneNumber());
+			return pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+}
