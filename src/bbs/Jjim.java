09d929c809f7c0044dce4feb79e99f314cbc16c6
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-14 17:11:21 +0900

    Create Jjim.java

---
+++ src/bbs/Jjim.java
@@ -0,0 +1,18 @@
+package bbs;
+
+public class Jjim {
+	private int bbsID;
+	private String userID;
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+}
