b3d491ab528810765961478e36bc74d819c901d7
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-28 18:10:37 +0900

    Create Bbs.java

---
+++ src/bbs/Bbs.java
@@ -0,0 +1,62 @@
+package bbs;
+
+public class Bbs {
+	private int boardID;
+	private int bbsID;
+	private String bbsTitle;
+	private String userID;
+	private String bbsDate;
+	private String bbsContent;
+	private String map;
+	private int bbsAvailable;
+	public int getBoardID() {
+		return boardID;
+	}
+	public void setBoardID(int boardID) {
+		this.boardID = boardID;
+	}
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getBbsTitle() {
+		return bbsTitle;
+	}
+	public void setBbsTitle(String bbsTitle) {
+		this.bbsTitle = bbsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getBbsDate() {
+		return bbsDate;
+	}
+	public void setBbsDate(String bbsDate) {
+		this.bbsDate = bbsDate;
+	}
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	public String getMap() {
+		return map;
+	}
+	public void setMap(String map) {
+		this.map = map;
+	}
+	public int getBbsAvailable() {
+		return bbsAvailable;
+	}
+	public void setBbsAvailable(int bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+	
+	
+}
