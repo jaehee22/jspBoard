eec272cf24d22428baa14c38b256aa5a211b6d6a
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-28 18:10:40 +0900

    Create Evaluation.java

---
+++ src/evaluation/Evaluation.java
@@ -0,0 +1,41 @@
+package evaluation;
+
+public class Evaluation {
+	private int bbsID;
+	private String userID;
+	private int likeEat;
+	private int sosoEat;
+	private int badEat;
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public int getLikeEat() {
+		return likeEat;
+	}
+	public void setLikeEat(int likeEat) {
+		this.likeEat = likeEat;
+	}
+	public int getSosoEat() {
+		return sosoEat;
+	}
+	public void setSosoEat(int sosoEat) {
+		this.sosoEat = sosoEat;
+	}
+	public int getBadEat() {
+		return badEat;
+	}
+	public void setBadEat(int badEat) {
+		this.badEat = badEat;
+	}
+	
+	
+}
